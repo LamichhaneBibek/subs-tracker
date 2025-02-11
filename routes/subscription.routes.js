762a25f5cc4964c1c7fac3dbf5864da0f16664b9
LamichhaneBibek <lamichhane.kebib@gmail.com>
2025-02-11 09:17:11 +0545

    Add subscription routes for managing subscriptions and retrieving user-specific data

---
+++ routes/subscription.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get("/", (req, res) => res.send({ title: "Get all subscriptions" }));
+
+subscriptionRouter.get("/:id", (req, res) => res.send({ title: "Get subscription by id" }));
+
+subscriptionRouter.post("/", authorize, createSubscription);
+
+subscriptionRouter.put("/:id", (req, res) => res.send({ title: "Update subscription" }));
+
+subscriptionRouter.delete("/:id", (req, res) => res.send({ title: "Delete subscription" }));
+
+subscriptionRouter.get("/user/:id", authorize,getUserSubscriptions);
+
+subscriptionRouter.put("/:id/cancel", (req, res) => res.send({ title: "Cancel subscription by id" }));
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) => res.send({ title: "Get upcoming renewals" }));
+
+
+export default subscriptionRouter;
